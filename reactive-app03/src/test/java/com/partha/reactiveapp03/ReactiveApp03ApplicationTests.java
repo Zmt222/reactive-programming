2364b959232613e11b428df088b72b7c7c080652
biswaspa <biswaspa@vmware.com>
2020-04-02 13:15:43 +0530

    reactive programming with functional-web

---
+++ reactive-app03/src/test/java/com/partha/reactiveapp03/ReactiveApp03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partha.reactiveapp03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApp03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
