82d5c064af049f5bf8aae5b340350a37e90a4e1f
biswaspa <biswaspa@vmware.com>
2020-04-05 20:13:00 +0530

    functional web apis and their test cases added

---
+++ reactive-app06/src/main/java/com/partha/reactiveapp06/ReactiveApp06Application.java
@@ -0,0 +1,13 @@
+package com.partha.reactiveapp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApp06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApp06Application.class, args);
+	}
+
+}
---
+++ reactive-app06/src/test/java/com/partha/reactiveapp06/ReactiveApp06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partha.reactiveapp06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApp06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
